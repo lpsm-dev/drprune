be97a5fa031eb352439eb3c5c6e299054bc9ad2d
lpmatos <ci-monk@protonmail.com>
2022-01-10 02:14:35 -0300

    feat: add upgrade setup

--- commands/upgrade.go
+++ commands/upgrade.go
@@ -1,1 +1,26 @@
 package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// UpgradeCmd downloads and installs the most recent version of the CLI.
+var UpgradeCmd = &cobra.Command{
+	Use:     "upgrade",
+	Aliases: []string{"u"},
+	Short:   "Upgrade to the latest version of the CLI.",
+	Long: `Upgrade to the latest version of the CLI.
+
+This finds and downloads the latest release, if you don't
+already have it.
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Upgrade")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(UpgradeCmd)
+}
